29277499393e8c92c70eae90e860b7b23f33b1a4
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-15 01:44:58 +0600

    Added javascript to show and hide the modal

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalButton = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeButton = document.querySelector('.close-btn');
+
+modalButton.addEventListener('click', () => {
+  modalOverlay.classList.add('open-modal');
+});
+
+closeButton.addEventListener('click', () => {
+  modalOverlay.classList.remove('open-modal');
+});
