d638d73e58d18a6b029122a2567490c18228b907
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-15 21:00:55 +0600

    solve 2 by selectors

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,11 +1,20 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach((btn) => {
-  btn.addEventListener('click', (x) => {
-    const question = x.currentTarget.parentElement.parentElement;
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (x) => {
+//     const question = x.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
     question.classList.toggle('show-text');
   });
 });
