58e13fd90bb4a8778b81e8ce8356da318456778a
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-15 20:56:14 +0600

    solve 1 by dom traversing

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (x) => {
+    const question = x.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
