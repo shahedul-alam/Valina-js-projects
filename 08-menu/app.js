cbdb2f03c12f25527ca6116fd366ca77975afa1f
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-15 22:52:05 +0600

    method 1 for showing menu contents on load

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -72,3 +72,27 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+let allContent = '';
+menu.forEach((item) => {
+  allContent += `
+    <article class="menu-item">
+          <img src="${item.img}" class="photo" alt="menu item">
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+            ${item.desc}
+            </p>
+          </div>
+        </article>
+  `
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  sectionCenter.innerHTML = allContent;
+});
