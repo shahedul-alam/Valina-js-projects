8a64f5e3c6bd19134da646aba1eb0992c832f778
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-27 15:01:31 +0600

    show more image when clicked on showMore btn

--- 25-image-search-engine/script.js
+++ 25-image-search-engine/script.js
@@ -10,12 +10,14 @@ let page = 1;
 async function searchImages()
 {
   keyword = searchBox.value;
-  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(URL);
   const data = await response.json();
-
   const results = data.results;
+
+  if(page === 1)
+    searchResult.innerHTML = '';
 
   results.map((result) => {
     const image = document.createElement('img');
@@ -25,10 +27,16 @@ async function searchImages()
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   });
+  showMoreBtn.style.display = 'block';
 }
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   page = 1;
+  searchImages();
+});
+
+showMoreBtn.addEventListener('click', () => {
+  page++;
   searchImages();
 });
