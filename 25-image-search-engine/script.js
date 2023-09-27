69d8f8e6e08642f223aaec814a929c6f93153418
shahedul-alam <shaheedalamcontact@gmail.com>
2023-09-27 14:38:27 +0600

    fetch API with keyword & page no.

---
+++ 25-image-search-engine/script.js
@@ -0,0 +1,17 @@
+const accessKey = 'haW38azUGxpJgyNpzH4XrtLj0T4yYxifA7pnwlAhoWU';
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-button');
+
+let keyword = '';
+let page = 1;
+
+async function searchImages()
+{
+  keyword = searchBox.value;
+  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(URL);
+  const data = await response.json();
+}
